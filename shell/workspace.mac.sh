732d53a08cf07619737c6fb0a7ac3e4429d39fda
Qin Xiao <xiaoqin@caicloud.io>
2018-07-04 00:24:52 +0800

    2018.07.04

--- shell/workspace.mac.sh
+++ shell/workspace.mac.sh
@@ -2,10 +2,11 @@ export WSPACE=$HOME'/workspace'
 cw() {
 	cd $WSPACE/$1
 }
+alias ccai="cw cc/src/github.com/caicloud"
 
 # [require code]
 alias cwl="ls -l $WSPACE/dotfiles/code-workspace"
-alias c.='code .'
+alias c.="code ."
 cwc() {
 	fn=$WSPACE/dotfiles/code-workspace/$1.code-workspace
 	echo "opening $fn ..."
